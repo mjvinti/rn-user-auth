ea003f7536da19d8b480fa4729c80f043ba056f4
mjvinti <mjvinti@gmail.com>
2023-11-22 16:41:05 -0500

    accessing protected routes

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+import { AuthContext } from '../store/authContext';
 
 function WelcomeScreen() {
+  const { token } = useContext(AuthContext);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-course-e998d-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((response) => setMessage(response.data));
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
