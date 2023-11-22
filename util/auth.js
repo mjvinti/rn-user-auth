37b790c0ce6a27de14874a52ad2e036459dbc032
mjvinti <mjvinti@gmail.com>
2023-11-22 14:52:47 -0500

    logging users in

--- util/auth.js
+++ util/auth.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${process.env.EXPO_PUBLIC_API_KEY}`;
+
+  return await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true
+  });
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.EXPO_PUBLIC_API_KEY}`,
-    { email, password, returnSecureToken: true }
-  );
+  await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }
